44e2d96602db2e81b287b15b3d6003397b8623f8
Miguel Londoño <magnolinneo1971@gmail.com>
2026-02-27 20:37:33 -0500

    creamos la clase persona

---
+++ src/main/java/com/example/Persona.java
@@ -0,0 +1,50 @@
+package com.example;
+
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private int numeroDocumento;
+    private int añoNacimiento;
+
+    public Persona(String nombre, String apellido, int numeroDocumento, int añoNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numeroDocumento = numeroDocumento;
+        this.añoNacimiento = añoNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(int numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public int getAñoNacimiento() {
+        return añoNacimiento;
+    }
+
+    public void setAñoNacimiento(int añoNacimiento) {
+        this.añoNacimiento = añoNacimiento;
+    }
+
+
+}
